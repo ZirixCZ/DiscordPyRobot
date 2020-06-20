0f4c0fed985f5d41ca7ce32e568ba283ff6646d5
ZirixCZ <49836430+ZirixCZ@users.noreply.github.com>
2020-06-20 23:23:52 +0200

    Ive added the baseline
    pog mat is gay COCK

--- source.py
+++ source.py
@@ -1,3 +1,18 @@
-#hello world
-#litteraly best discord bot on the world
-#ofc bro u dumb
+import os
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("The bot is ready!")
+    client.change_presence(game=discord.Game(name="Hellow world"))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content == "Hello":
+        await client.send_message(message.channel, "World")
+
+TOKEN = os.getenv("NzI0MDAyODMzMTQ5NDYwNTkx.Xu58Aw.Guk_uwMxOFC5oqcqnzdNoAQcAnU")
