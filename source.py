f89a6d4087aa31c9688cbfbf61e0dca7b25f68e2
ZirixCZ <49836430+ZirixCZ@users.noreply.github.com>
2020-06-21 16:22:52 +0200

    updated the file
    přidal jsem popisky a přidal jsem command.

--- source.py
+++ source.py
@@ -1,18 +1,23 @@
 import os
 import discord
+
+#this is our secret token ok, dont loose it
 TOKEN = "NzI0MDAyODMzMTQ5NDYwNTkx.Xu58Aw.Guk_uwMxOFC5oqcqnzdNoAQcAnU"
 client = discord.Client()
 
+#this will tell us if the bot connected to the server (respektive jestli máme správný token omegalulw)
 @client.event
 async def on_ready():
     print("The bot is ready!")
-    client.change_presence(game=discord.Game(name="Hellow world"))
+    await client.change_presence(activity=discord.Game("yo wuddap"))
+
 
 @client.event
-async def on_message(message):
-    if message.author == client.user:
+async def on_message(message): #this will take the on_message function from discord lib and make it into def so we can use it mweeeeew
+
+    if message.author == client.user: #this will check if the sender is someone else than the acctual client (nebude odpovídat na své zprávy - zamezí se loopům)
         return
     if message.content == "Hello":
-        await client.send_message(message.channel, "World")
+        await message.channel.send("world") #line 20 až 21 je simple send message command
 
-client.run(TOKEN)
+client.run(TOKEN) #připojí se na token přes discord api
