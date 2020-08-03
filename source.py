2d5bfd4efe1af1efebb18516e7b304ae614cd42d
Pero-coder <67205912+Pero-coder@users.noreply.github.com>
2020-08-03 16:59:36 +0200

    Changing status

--- source.py
+++ source.py
@@ -1,19 +1,29 @@
 import os
+import asyncio
 import discord
 from discord.ext import commands
 import random
 
 client = commands.Bot(command_prefix = 'getme ')
+token = "NzI0MDAyODMzMTQ5NDYwNTkx.Xu58Aw.Guk_uwMxOFC5oqcqnzdNoAQcAnU" #this is our secret token ok, dont loose it
+
+async def status_task():
+    while True:
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="getme help"))
+        await asyncio.sleep(10)
+        await client.change_presence(activity=discord.Game("Created by:"))
+        await asyncio.sleep(2)
+        await client.change_presence(activity=discord.Game("Péro, Zirix, SNICKERS"))
+        await asyncio.sleep(3)
 
 @client.event
 async def on_ready():
     print("The bot is ready!")
-    await client.change_presence(activity=discord.Game("yo wuddap"))
+    await client.loop.create_task(status_task())
 
 
-for filename in os.listdir(r'discord\membot\cogs'):
+for filename in os.listdir(r'python\discord\membot\cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-#this is our secret token ok, dont loose it
-client.run("NzI0MDAyODMzMTQ5NDYwNTkx.Xu58Aw.Guk_uwMxOFC5oqcqnzdNoAQcAnU") #připojí se na token přes discord api
+client.run(token) #připojí se na token přes discord api
